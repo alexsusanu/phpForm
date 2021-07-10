aea0c5a01efeea6b4444c647689918b6eb68f7f2
alex <alex@susanu.com>
2021-07-10 13:32:39 +0100

    finished front end

--- sample.php
+++ sample.php
@@ -6,75 +6,86 @@
     <title>AlexSusanu</title>
 </head>
 <body>
-    <div class="main">
-        <div class="subMain">
-            <div id="firstName">
-                <label for="firstName">First Name</label>
-                <input type="text" id="firstName"/>
+        <form>
+            <div class="subMain">
+                <div id="firstName">
+                    <label for="firstName">First Name</label>
+                    <input type="text" id="firstName"/>
+                </div>
+
+                <div id="lastName">
+                    <label for="lastName">Last Name</label>
+                    <input type="text" id="lastName">
+                </div>
             </div>
 
-            <div id="lastName">
-                <label for="lastName">Last Name</label>
-                <input type="text" id="lastName">
-            </div>
-        </div>
+            <div class="subMain">
+                <div id="emailAddress">
+                    <label for="emailAddress">Email Address</label>
+                    <input type="text" id="emailAddress">
+                </div>
 
-        <div class="subMain">
-            <div id="emailAddress">
-                <label for="emailAddress">Email Address</label>
-                <input type="text" id="emailAddress">
-            </div>
-
-            <div id="telephoneNumber">
-                <label for="telephoneNumber">Telephone Number</label>
-                <input type="text" id="telephoneNumber">
-            </div>
-        </div>
-
-        <div class="subMain">
-            <div id="addressOne">
-                <label for="addressOne">Address 1</label>
-                <input type="text" id="addressOne">
+                <div id="telephoneNumber">
+                    <label for="telephoneNumber">Telephone Number</label>
+                    <input type="text" id="telephoneNumber">
+                </div>
             </div>
 
-            <div id="addressTwo">
-                <label for="addressTwo">Address 2</label>
-                <input type="text" id="addressTwo">
-            </div>
-        </div>
+            <div class="subMain">
+                <div id="addressOne">
+                    <label for="addressOne">Address 1</label>
+                    <input type="text" id="addressOne">
+                </div>
 
-        <div class="subMain">
-            <div id="town">
-                <label for="town">Town</label>
-                <input type="text" id="town">
-            </div>
-
-            <div id="county">
-                <label for="county">County</label>
-                <input type="text" id="county">
-            </div>
-        </div>
-
-        <div class="subMain">
-            <div id="postcode">
-                <label for="postcode">Postcode</label>
-                <input type="text" id="postcode">
+                <div id="addressTwo">
+                    <label for="addressTwo">Address 2</label>
+                    <input type="text" id="addressTwo">
+                </div>
             </div>
 
-            <div id="country"><label for="country">Country</label>
-                <select id="country">
-                    <?php
-                    $countries = file_get_contents('countries.json');
-                    $json = json_decode($countries, true);
-                    foreach ($json as $item) {
-                        echo "<option>";
-                        echo $item['name'];
-                        echo "</option>'";
-                    }
-                    ?>
-                </select>
+            <div class="subMain">
+                <div id="town">
+                    <label for="town">Town</label>
+                    <input type="text" id="town">
+                </div>
+
+                <div id="county">
+                    <label for="county">County</label>
+                    <input type="text" id="county">
+                </div>
             </div>
-        </div>
-    </div>
+
+            <div class="subMain">
+                <div id="postcode">
+                    <label for="postcode">Postcode</label>
+                    <input type="text" id="postcode">
+                </div>
+
+                <div id="country"><label for="country">Country</label>
+                    <select id="country">
+                        <?php
+                        $countries = file_get_contents('countries.json');
+                        $json = json_decode($countries, true);
+                        foreach ($json as $item) {
+                            echo "<option>";
+                            echo $item['name'];
+                            echo "</option>'";
+                        }
+                        ?>
+                    </select>
+                </div>
+            </div>
+            <div id="description">
+                <label for="description">Description</label>
+                <textarea cols="10" rows="10"></textarea>
+            </div>
+            <div id="browseFile">
+                <p>Your C.V</p>
+                <p>Select a file &nbsp;<input type="file" value="Browse"></p>
+            </div>
+            <div id="submit">
+                <input type="submit" value="Submit">
+            </div>
+        </form>
 </body>
 </html>
